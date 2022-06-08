a10e845f5eadb5035543dbdbe52a5715b3cec78b
Haris Grbo <haris@misijaweb.com>
2022-06-08 20:30:16 +0200

    fix

--- pages/index-employer.vue
+++ pages/index-employer.vue
@@ -116,7 +116,7 @@
                 <div class="review-cards-wrapper">
                     <ReviewCard @open-review-card-modal="handleOpenReviewCardInModal(user)" :user="user"
                                 v-for="(user, index) in reviewUsers.slice(0, 5)" :key="index"
-                                v-show="user.description.length"
+                                v-show="user.description && user.description.length"
                     ></ReviewCard>
                 </div>
                 <client-only>
